63c3dd22f555ce5fc50c44abf9e8fc7c715f1eda
Maxime <maxime.bidan33@gmail.com>
2024-04-15 09:01:32 +0200

    add comments

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+/**GOAL :
+Vous décidez de participer à des concours avec comme gain des sommes d'argent.
+
+Vous savez que :
+
+- Par jour il n'y a qu'un seul concours
+- Les concours sont consécutifs
+- Tous les concours ne rapport pas la même somme
+- Il y a N concours consécutifs
+- Les concours commencent toujours le 1
+
+Vous voudriez gagner un maximum en participant à tous les concours possible seulement au bout de X concours consécutifs vous êtes fatigué et vous vous reposez un jour.
+
+Ecrivez un programme qui affiche les gains maximum en respectant les règles au-dessus.
+
+**Exemple:**
+
+Il y a 10 concours
+
+Vous pouvez jouer 4 concours consécutifs
+
+Les gains possibles sont 13, 2, 15, 17, 19, 33, 2, 2, 2, 2.
+
+Donc, vous jouerez les jours :  **1** > **3** > **4** > **5** > **6** > **8** > **9** > **10**
+
+Ainsi, vous gagnerez 103 💵*/
+
 //FUNCTION PRINCIPALE
 
 function getGain(listeGains, nbrWorkday) {
